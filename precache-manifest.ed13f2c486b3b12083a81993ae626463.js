f8f6259ccc42d48e0a4fa73663ef41c23ea42b9c
MateusMP <mateus.mp93@gmail.com>
2020-11-15 18:53:02 -0400

    Deploy

---
+++ precache-manifest.ed13f2c486b3b12083a81993ae626463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32b0b32e423ca0bc7e77bddf2500eee",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "b168c0da7e9e52273384",
+    "url": "/Website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4da336a4d017ebadcd36",
+    "url": "/Website/static/css/main.43fcc9fc.chunk.css"
+  },
+  {
+    "revision": "b168c0da7e9e52273384",
+    "url": "/Website/static/js/2.4e4a5a37.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Website/static/js/2.4e4a5a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da336a4d017ebadcd36",
+    "url": "/Website/static/js/main.14976b01.chunk.js"
+  },
+  {
+    "revision": "3ae78e46aafcf7ec3072",
+    "url": "/Website/static/js/runtime-main.9f5203b6.js"
+  }
+]);
