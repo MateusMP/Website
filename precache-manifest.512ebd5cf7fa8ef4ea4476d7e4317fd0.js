71d55365d317e9dd6529d1c2dbedd09683015141
MateusMP <mateus.mp93@gmail.com>
2022-06-11 11:26:20 -0300

    Deploy

---
+++ precache-manifest.512ebd5cf7fa8ef4ea4476d7e4317fd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0dfbe20adff272736076d40928f6d0",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "af384e2b8364075fc2e1",
+    "url": "/Website/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "6e999d15c9545825196c",
+    "url": "/Website/static/css/main.43fcc9fc.chunk.css"
+  },
+  {
+    "revision": "af384e2b8364075fc2e1",
+    "url": "/Website/static/js/2.da18f6b2.chunk.js"
+  },
+  {
+    "revision": "6cfffc21677fea4ccac09045cec2b890",
+    "url": "/Website/static/js/2.da18f6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e999d15c9545825196c",
+    "url": "/Website/static/js/main.e75be8f2.chunk.js"
+  },
+  {
+    "revision": "3ae78e46aafcf7ec3072",
+    "url": "/Website/static/js/runtime-main.9f5203b6.js"
+  }
+]);
